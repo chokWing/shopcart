c768cb7f68a80c21c3b91eae3727744bf459d6ae
neoboy <neoboy@163.com>
2018-01-19 18:09:25 +0800

    删除itemTLD导致数组下标错乱BUG；

--- js/shopcart.js
+++ js/shopcart.js
@@ -1,14 +1,66 @@
-new Vue({
-    el:'#app',
-    data:{
-        showFlag:false,
-    },
-    mounted () {
-
-        },
-    methods: {
-        toggle () {
-            this.showFlag=!this.showFlag;
+window.onload=function () {
+//点击编辑事件
+    let itemLTD = document.getElementsByClassName('item-LTD'); //按商家获取
+    for (let i = 0; i < itemLTD.length; i++) {
+        let edit_a = itemLTD[i].getElementsByClassName('edit')[0];
+        edit_a.onclick = function (flag) {
+            if (flag === event) {
+                this.innerText = this.innerText === '编辑' ? '完成' : '编辑';
+            } else {
+                this.innerText = flag;
+            }
+            let pannel = itemLTD[i].getElementsByClassName('info');
+            if (this.innerText === '完成') {
+                for (let j = 0; j < pannel.length; j++) {
+                    pannel[j].style.display = 'none';
+                    pannel[j].nextElementSibling.style.display = 'flex';
+                }
+            } else {
+                for (let j = 0; j < pannel.length; j++) {
+                    pannel[j].style.display = 'block';
+                    pannel[j].nextElementSibling.style.display = 'none';
+                }
+            }
         }
     }
-})
+//顶部编辑
+    let topEdit = document.getElementById('cart-top').getElementsByTagName('a')[0];
+    topEdit.onclick = function () {
+        this.innerText = this.innerText === '编辑' ? '完成' : '编辑';
+        let flag = this.innerText;
+        for (let i = 0; i < itemLTD.length; i++) {
+            let edit_a = itemLTD[i].getElementsByClassName('edit')[0];
+            edit_a.onclick(flag);
+            if (this.innerText === '编辑') {
+                edit_a.style.display = 'flex';
+            } else {
+                edit_a.style.display = 'none';
+            }
+        }
+    }
+    /*商品数量增减以及删除功能*/
+    let info2 = document.getElementsByClassName('info2'); //获取所有的可编辑面板
+    for (let i = 0; i < info2.length; i++) {
+        info2[i].onclick = function (e) {
+            let el = e.srcElement;
+            let cls = el.className;
+            let input = this.getElementsByTagName('input')[0];
+            let item=this.parentNode.parentNode.parentNode;  //商品
+            switch (cls) {
+                case 'add':
+                    input.value++;
+                    break;
+                case 'decrease':
+                    if(input.value>1){
+                        input.value--;
+                    }
+                    break;
+                case 'del':
+                    if(item.parentNode.childElementCount===1){
+                        item.parentNode.parentNode.parentNode.removeChild(item.parentNode.parentNode);
+                    }
+                    item.parentNode.removeChild(item);
+            }
+        }
+    }
+}
